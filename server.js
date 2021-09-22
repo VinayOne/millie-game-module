891cc70179661de3b0e08a18b02beaad6e945552
Brandon Earwood <earwood.brandon@gmail.com>
2021-09-22 15:21:50 -0500

    Changing src to dist

--- server.js
+++ server.js
@@ -20,16 +20,16 @@ mongoose.connect(url,connectionParams)
         app.use(cors());
 		app.use(bodyParser.json());
 
-		app.use(express.static(path.join(__dirname, "src")));
+		app.use(express.static(path.join(__dirname, "dist")));
 
 		app.use("/api", routes);
 
 		app.get('', (req, res) => {
-		  res.sendFile(path.join(__dirname, "src", "index.html"));
+		  res.sendFile(path.join(__dirname, "dist", "index.html"));
 		});
 
 		app.get('/', (req, res) => {
-		  res.sendFile(path.join(__dirname, "src", "index.html"));
+		  res.sendFile(path.join(__dirname, "dist", "index.html"));
 		});
 
 		const port = process.env.PORT || 3000;
