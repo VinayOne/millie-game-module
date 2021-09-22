4783b1437e4aa63795a860501bd83f8d902bc5db
Brandon Earwood <earwood.brandon@gmail.com>
2021-09-22 11:57:57 -0500

    More corrections to set up production mode

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ mongoose.connect(url,connectionParams)
         app.use(cors());
 		app.use(bodyParser.json());
 
-		app.use(express.static(path.join(__dirname, "src")));
-
-		app.get('*', (req, res) => {
-		  res.sendFile(path.join(__dirname, "src/index.html"));
-		});
+		app.use(express.static(path.join(__dirname, "/src")));
 
 		app.use("/api", routes);
+
+		app.get('*', (req, res) => {
+		  res.sendFile(path.join(__dirname, "/src/index.html"));
+		});
 
 		const port = process.env.PORT || 3000;
 
