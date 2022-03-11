b555af94a8ac64eff08dc56850dbf2a2b03fa1c2
vinayone <vinaymunda@outlook.com>
2022-03-11 17:49:35 +0530

    inserted php files

---
+++ php/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = "YOUR_DB_USER"; // Mysql username 
+$db_password = "YOUR_DB_PWD"; // Mysql password 
+$db_name = "YOUR_DB_NAME"; // Database name 
+$db_table = "scores"; // Table name
+
+?>
